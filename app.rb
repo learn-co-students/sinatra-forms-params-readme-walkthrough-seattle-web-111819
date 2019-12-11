c8b53dacb1de4dfecfadb83de16c480253d837df
Alice Won <alicewon4@gmail.com>
2019-12-10 19:33:36 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+#this is the application CONTROLLER
+#need to tell form WHERE and HOW to send data from the user, give this in <form> tag
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -7,5 +10,15 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+
+  get '/' do
+    "Hello World"
+  end
+
+  post '/food' do
+    # params.to_s
+
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 
 end
